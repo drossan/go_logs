d3878d2123c26e7b24a648f6b3338b55f0902ab2
Daniel Rosselló <drossan@hotmail.es>
2024-05-03 11:47:26 +0200

    feat: Add centralized logging and Slack notification support
    The package has been refactored to centralise logging and bug tracking. This now allows the application to handle logging of various severity levels and supports notification via Slack.

--- logs.go
+++ logs.go
@@ -3,24 +3,11 @@ package go_logs
 import (
 	"github.com/fatih/color"
 	"log"
-	"os"
 )
-
-func saveLog(message string) {
-	file, err := os.OpenFile("logs.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func(file *os.File) {
-		_ = file.Close()
-	}(file)
-	logger := log.New(file, "", log.LstdFlags)
-	logger.Println(message)
-}
 
 func FatalLog(message string) {
 	color.Set(color.FgRed)
-	saveLog(" 💣 " + message)
+	saveLog(" 💣 "+message, "FATAL")
 	color.Unset()
 	log.Fatal(" 💣  " + message)
 }
@@ -29,19 +16,19 @@ func ErrorLog(message string) {
 	color.Set(color.FgRed)
 	log.Println(" 🚨 ", message)
 	color.Unset()
-	saveLog(" 🚨 " + message)
+	saveLog(" 🚨 "+message, "ERROR")
 }
 
 func InfoLog(message string) {
 	color.Set(color.FgYellow)
 	log.Println(" ⚠️ ", message)
 	color.Unset()
-	saveLog(" ⚠️  " + message)
+	saveLog(" ⚠️  "+message, "INFO")
 }
 
 func SuccessLog(message string) {
 	color.Set(color.FgGreen)
 	log.Println(" ✅ ", message)
 	color.Unset()
-	saveLog(" ✅ " + message)
+	saveLog(" ✅ "+message, "SUCCESS")
 }
